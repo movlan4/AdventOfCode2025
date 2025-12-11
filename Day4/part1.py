6a80fb0d8d0a5a5f01cf1f1c0f356a0c3f37884b
Mövlan <emenovmovlan@gmail.com>
2025-12-11 23:55:13 +0400

    Day 4

---
+++ Day4/part1.py
@@ -0,0 +1,38 @@
+data=[]
+with open("input.txt","r") as f:
+    for l in f:
+        now=[]
+        for i in l.strip():
+            if i=='.':
+                now.append(0)
+            else:
+                now.append(1)
+        data.append(now)
+n=len(data)
+m=len(data[0])
+res=0
+for i in range(n):
+    for j in range(m):
+        cnt=0
+        if data[i][j]==1:
+            if i!=0:
+                if j!=0:
+                    cnt+=data[i-1][j-1]
+                if j!=m-1:
+                    cnt+=data[i-1][j+1]
+                cnt+=data[i-1][j]
+            if j!=0:
+                cnt+=data[i][j-1]
+            if j!=m-1:
+                cnt+=data[i][j+1]
+            if i!=n-1:
+                if j!=0:
+                    cnt+=data[i+1][j-1]
+                if j!=m-1:
+                    cnt+=data[i+1][j+1]
+                cnt+=data[i+1][j]
+            if cnt<4:
+                res+=1
+
+print(res)
+
